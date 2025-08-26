f6e14095764cb09f5105e7bb2c3ccd67f2346bf1
GUSTAVO PEREIRA GARBIM <gustavo.garbim@websupply.com.br>
2025-08-26 13:38:08 -0300

    projeto avancado

---
+++ Course2/ContaBancaria.cs
@@ -0,0 +1,73 @@
+﻿public enum TipoConta
+{
+    Corrente,
+    Poupanca
+}
+
+class ContaBancaria
+{
+    // Campos (Atributos) Privados
+    private static int s_proximoNumeroConta = 1001; // "s_" é uma convenção para campos estáticos
+
+    // Propriedades (Acesso controlado aos dados)
+    public int NumeroConta { get; private set; }
+    public string Titular { get; set; }
+    public double Saldo { get; private set; }
+    public TipoConta Tipo { get; private set; }
+
+    // Construtor
+    public ContaBancaria(string titular, TipoConta tipo, double saldoInicial = 0)
+    {
+        // Atribui um número de conta único e o incrementa para a próxima conta
+        NumeroConta = s_proximoNumeroConta;
+        s_proximoNumeroConta++;
+
+        Titular = titular;
+        Tipo = tipo;
+        Saldo = saldoInicial; // O saldo inicial é depositado aqui
+    }
+
+    // Métodos
+    public void Depositar(double valor)
+    {
+        if (valor > 0)
+        {
+            Saldo += valor;
+            Console.WriteLine($"Depósito de R${valor:F2} realizado com sucesso.");
+        }
+        else
+        {
+            Console.WriteLine("O valor do depósito deve ser positivo.");
+        }
+    }
+
+    // Este método agora retorna 'true' ou 'false' para indicar o sucesso da operação.
+    // Isso é crucial para a lógica de transferência!
+    public bool Sacar(double valor)
+    {
+        // Adicionamos uma taxa de R$5.00 para saques de conta corrente
+        double taxaSaque = (this.Tipo == TipoConta.Corrente) ? 5.00 : 0;
+
+        if (valor <= 0)
+        {
+            Console.WriteLine("O valor do saque deve ser positivo.");
+            return false;
+        }
+
+        if ((Saldo - valor - taxaSaque) < 0)
+        {
+            Console.WriteLine("Saldo insuficiente.");
+            return false;
+        }
+
+        Saldo -= (valor + taxaSaque);
+        Console.WriteLine($"Saque de R${valor:F2} realizado com sucesso (Taxa: R${taxaSaque:F2}).");
+        return true;
+    }
+
+    // Sobrescrevendo ToString para uma exibição clara dos dados da conta
+    public override string ToString()
+    {
+        return $"Conta: {NumeroConta} | Tipo: {Tipo} | Titular: {Titular} | Saldo: R${Saldo:F2}";
+    }
+}
---
+++ Course2/Program.cs
@@ -0,0 +1,181 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Xml.Linq;
+
+/*Pedi um desafio um pouco mais avançado para POO baseado no outro sistema bancário, o desafio foi criar a lógica do sistema do 0 e implementar os métodos corretamente.*/
+
+namespace SistemaBancario
+{
+    class Program
+    {
+        static List<ContaBancaria> contas = new List<ContaBancaria>();
+
+        static void Main(string[] args)
+        {
+            string opcaoUsuario = "";
+            while (opcaoUsuario.ToUpper() != "S")
+            {
+                ExibirMenu();
+                opcaoUsuario = Console.ReadLine().ToUpper();
+
+                switch (opcaoUsuario)
+                {
+                    case "1":
+                        CriarConta();
+                        break;
+                    case "2":
+                        ListarContas();
+                        break;
+                    case "3":
+                        RealizarDeposito();
+                        break;
+                    case "4":
+                        RealizarSaque();
+                        break;
+                    case "5":
+                        RealizarTransferencia();
+                        break;
+                    case "L":
+                        Console.Clear();
+                        break;
+                    case "S":
+                        Console.WriteLine("Obrigado por utilizar nossos serviços.");
+                        break;
+                    default:
+                        Console.WriteLine("Opção inválida. Tente novamente.");
+                        break;
+                }
+                Console.WriteLine("Pressione qualquer tecla para continuar...");
+                Console.ReadKey();
+            }
+        }
+
+
+
+        private static void ExibirMenu() {
+            Console.WriteLine("Selecione a opção 1 para Criar Conta.");
+            Console.WriteLine("Selecione a opção 2 para Listar Contas.");
+            Console.WriteLine("Selecione a opção 3 para Realizar Deposito.");
+            Console.WriteLine("Selecione a opção 4 para Realizar Saque.");
+            Console.WriteLine("Selecione a opção 5 para Realizar Transferencia.");
+            Console.WriteLine("Selecione a opção L para Limpar o Console.");
+            Console.WriteLine("Selecione a opção S para fechar nosso sistema.");
+        }
+        private static void CriarConta()
+        {
+            Console.WriteLine("--- Criação de Nova Conta ---");
+
+            Console.Write("Digite o Nome do Titular: ");
+            string nome = Console.ReadLine();
+
+            Console.Write("Digite o tipo de conta (0 para Corrente, 1 para Poupança): ");
+            int tipoSelecionadoInt = int.Parse(Console.ReadLine());
+
+            TipoConta tipoConta;
+            if (tipoSelecionadoInt == 1)
+            {
+                tipoConta = TipoConta.Poupanca;
+            }
+            else
+            {
+                tipoConta = TipoConta.Corrente;
+            }
+
+            Console.Write("Deseja fazer um depósito inicial? (s/n): ");
+            string resp = Console.ReadLine().ToLower();
+
+            double saldoInicial = 0;
+            if (resp == "s")
+            {
+                Console.Write("Digite o valor do depósito inicial: R$");
+                saldoInicial = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            }
+
+            ContaBancaria novaConta = new ContaBancaria(nome, tipoConta, saldoInicial);
+
+            contas.Add(novaConta);
+
+            Console.WriteLine("Conta criada com sucesso!");
+            Console.WriteLine(novaConta);
+        }
+        private static void ListarContas() {
+            if (contas.Count == 0)
+            {
+                Console.WriteLine("A lista de contas está vazia!");
+            }
+            else
+            {
+                foreach (var conta in contas)
+                {
+                    Console.WriteLine(conta);
+                }
+            }
+        }
+        private static void RealizarDeposito() {
+            Console.Write("Digite o numero da conta do deposito: ");
+            int num = int.Parse(Console.ReadLine());
+            ContaBancaria contaParaDepositar = EncontrarConta(num);
+            if (contaParaDepositar == null)
+            {
+                Console.WriteLine("Conta não encontrada!");
+            }
+            else
+            {
+                Console.Write("Digite o valor para deposito: R$");
+                double valor2 = double.Parse(Console.ReadLine());
+                contaParaDepositar.Depositar(valor2);
+            }
+        }
+        private static void RealizarSaque(){
+            Console.Write("Digite o numero da conta de saque: ");
+            int num = int.Parse(Console.ReadLine());
+            ContaBancaria contaParaSacar = EncontrarConta(num);
+            if (contaParaSacar == null)
+            {
+                Console.WriteLine("Conta não encontrada!");
+            }
+            else
+            {
+                Console.Write("Digite o valor para saque: R$");
+                double valor2 = double.Parse(Console.ReadLine());
+                contaParaSacar.Sacar(valor2);
+            }
+        }
+        private static void RealizarTransferencia() {
+            Console.WriteLine("------Transferencia bancaria------");
+            Console.Write("Digite o numero da conta de Origem: ");
+            int num1 = int.Parse(Console.ReadLine());
+
+            Console.Write("Digite o valor da transferencia: ");
+            double valor1 = double.Parse(Console.ReadLine());
+
+            Console.Write("Digite o numero da conta de Destino: ");
+            int num2 = int.Parse(Console.ReadLine());
+
+            ContaBancaria contaOrigemTrans = EncontrarConta(num1);
+            ContaBancaria contaDestinoTrans = EncontrarConta(num2);
+
+            if (contaOrigemTrans == null && contaDestinoTrans == null)
+            {
+                Console.WriteLine("Conta não encontrada!");
+            }
+            else
+            {
+                Console.Write("Digite o valor a ser transferido: ");
+                contaOrigemTrans.Sacar(valor1);
+                contaDestinoTrans.Depositar(valor1);
+            }
+        }
+        private static ContaBancaria EncontrarConta(int numeroConta) {
+            foreach (var i in contas)
+            {
+                if (i.NumeroConta == numeroConta)
+                {
+                    return i;
+                }
+            }
+            return null;
+        }
+    }
+}
