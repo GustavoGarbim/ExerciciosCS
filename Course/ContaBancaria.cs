0073fc4a46ee6c685d9d2cb7f99bdf212c77bdac
74840 <74840@WEB-076>
2025-08-26 09:23:48 -0300

    Adicionar arquivos de projeto.

---
+++ Course/ContaBancaria.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Globalization;
+using System.Security.Cryptography.X509Certificates;
+namespace Course
+{
+    class ContaBancaria()
+    {
+        public string Nome;
+        public string NumConta;
+        public float SaldoConta;
+
+
+        public void AdicionarNome(string nome)
+        {
+            Nome = nome;
+        }
+
+        public void AdicionarNumConta(string numConta) {
+            NumConta = numConta;
+        }
+
+        public void AdicionarSaldoConta(int saldoConta)
+        {
+            SaldoConta = saldoConta;
+        }
+
+        public void DepositoConta(float depConta)
+        {
+            SaldoConta += depConta;
+        }
+
+        public void SaqueConta(float saqueConta)
+        {
+            SaldoConta -= saqueConta + 5;
+        }
+    }
+}
---
+++ Course/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Globalization;
+namespace Course
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ContaBancaria contaBancaria = new ContaBancaria();
+
+            Console.WriteLine("------GarbimBank------");
+
+            Console.Write("Entre com o número da conta: ");
+            string numconta = (Console.ReadLine());
+            contaBancaria.AdicionarNumConta(numconta); // método para adicionar numero da conta
+
+            Console.Write("Entre com o nome do proprietário da conta: ");
+            string nomeconta = Console.ReadLine();
+            contaBancaria.AdicionarNome(nomeconta); // método para adicionar nome na conta
+
+            Console.Write("Deseja adicionar um saldo inicial na conta? (s/n): ");
+            string perg = Console.ReadLine();
+
+            if (perg == "s")
+            {
+                Console.Write("Digite a quantia inicial: R$");
+                int resp = int.Parse(Console.ReadLine());
+                contaBancaria.AdicionarSaldoConta(resp); // método para adicionar saldo na conta
+            }
+            else
+            {
+                Console.WriteLine("Carregando...");
+            }
+            Console.WriteLine("-------------------------------");
+            Console.WriteLine($"Bem vindo {contaBancaria.Nome}!, sua agência é: {contaBancaria.NumConta} e seu saldo atual é de: {contaBancaria.SaldoConta}$");
+
+            Console.Write($"Entre com um valor para depósito sr(a) {contaBancaria.Nome}: R$:");
+            float dep = float.Parse(Console.ReadLine());
+            contaBancaria.DepositoConta(dep); // método para realizar deposito na conta
+
+            Console.WriteLine($"Valor total depositado: R$:{dep} | Valor Total conta: R$:{contaBancaria.SaldoConta}");
+
+            Console.Write("Entre com um valor de saque: ");
+            float saq = float.Parse(Console.ReadLine());
+            Console.WriteLine($"Saque de R$:{saq} realizado! Taxa de 5$ adicionada.");
+            contaBancaria.SaqueConta(saq); // método para retirar saldo da conta
+            Console.WriteLine("-------------------------------");
+            Console.WriteLine("Sistema finalizado!");
+            Console.WriteLine($"Dados atualizados: Nome: {contaBancaria.Nome}, Número bancário: {contaBancaria.NumConta}, Saldo da conta: R${contaBancaria.SaldoConta}");
+        }
+    }
+}
