c870448ae9a1b3e0c821aeaac16664bf523a07b5
GUSTAVO PEREIRA GARBIM <gustavo.garbim@websupply.com.br>
2025-08-27 14:54:33 -0300

    novas atividades2

---
+++ Course3/Program.cs
@@ -0,0 +1,21 @@
+﻿using Course;
+using Course3.Entidades;
+using Course3.Entidades.Enums;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Order order = new Order
+        {
+            Id = 1080,
+            Moment = DateTime.Now,
+            Status = Course3.Entidades.Enums.OrderStatus.PendingPayment
+        };
+
+        Console.WriteLine(order);
+        string txt = OrderStatus.PendingPayment.ToString();
+
+        Console.WriteLine(txt);
+    }
+}
